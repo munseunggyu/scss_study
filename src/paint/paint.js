a28d6a96b09305acfdae41ae0958bd3d299e829c
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-04-05 22:43:32 +0900

    Add paint page footer

--- src/paint/paint.js
+++ src/paint/paint.js
@@ -3,6 +3,7 @@ import './scss/style.scss'
 import {MdOutlineShoppingBag} from 'react-icons/md'
 function Paint(){
   return(
+    <>
     <div className='paint__container'>
       <header>
         <nav>
@@ -42,38 +43,76 @@ function Paint(){
               <button>BOOK A MAINCURE</button>
             </div>
           </div>
-        <div className='bottle__shop'>
-          <h5>THE SHOP</h5>
-          <h3>OUR Newest Polish</h3>
-          <div className='bottles__item__container'>
-            <div className='bottle__item__container'>
-              <img src='https://images.unsplash.com/photo-1618354691490-0de58a2c4c93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzcxfHxib3R0bGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' />
-              <h5>BOTTLE IS GOOD</h5>
-              <h3>LIKE FRENCH</h3>
-              <span>$22.00</span>
-              <button>ADD TO CART</button>
-            </div>
-            <div className='bottle__item__container'>
-              <img src='https://images.unsplash.com/photo-1615485736894-a2d2e6d4cd9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Ym90dGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
-              <h5>CUP IS GOOD</h5>
-              <h3>LIKE CANYON</h3>
-              <span>$24.00</span>
-              <button>ADD TO CART</button>
-            </div>
-            <div className='bottle__item__container'>
-              <div>
-              <img src='https://images.unsplash.com/photo-1618354691249-18772bbac3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGJvdHRsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' />
+          <div className='bottle__shop'>
+            <h5>THE SHOP</h5>
+            <h3>OUR Newest Polish</h3>
+            <div className='bottles__item__container'>
+              <div className='bottle__item__container'>
+                <img src='https://images.unsplash.com/photo-1618354691490-0de58a2c4c93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzcxfHxib3R0bGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' />
+                <h5>BOTTLE IS GOOD</h5>
+                <h3>LIKE FRENCH</h3>
+                <span>$22.00</span>
+                <button>ADD TO CART</button>
               </div>
-              <h5>BOTTLE IS GOOD</h5>
-              <h3>LIKE VELET</h3>
-              <span>$22.00</span>
-              <button>ADD TO CART</button>
+              <div className='bottle__item__container'>
+                <img src='https://images.unsplash.com/photo-1615485736894-a2d2e6d4cd9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Ym90dGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+                <h5>CUP IS GOOD</h5>
+                <h3>LIKE CANYON</h3>
+                <span>$24.00</span>
+                <button>ADD TO CART</button>
+              </div>
+              <div className='bottle__item__container'>
+                <div>
+                <img src='https://images.unsplash.com/photo-1618354691249-18772bbac3a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGJvdHRsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' />
+                </div>
+                <h5>BOTTLE IS GOOD</h5>
+                <h3>LIKE VELET</h3>
+                <span>$22.00</span>
+                <button>ADD TO CART</button>
+              </div>
             </div>
           </div>
-        </div>
+          <div className='grid__container'>
+            <img src='https://images.unsplash.com/photo-1541704328070-20bf4601ae3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c21hbGx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' />
+            <img src='https://images.unsplash.com/photo-1611772871226-9cc3057c4f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' /> 
+            <img src='https://images.unsplash.com/photo-1568568544477-ee88c0c1333e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+            <img src='https://images.unsplash.com/photo-1612613781524-1c3bc901d1bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+            <img src='https://images.unsplash.com/photo-1520575839349-52ff2532d910?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+            <img src='https://images.unsplash.com/photo-1591009112633-e07e1256bcc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+            <img src='https://images.unsplash.com/photo-1621076724832-a690d7906aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+            <img src='https://images.unsplash.com/photo-1612380318869-7925b91da6ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNtYWxsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' />
+          </div>
+          <div className='last__text'>
+            Good bye
+          </div>  
         </div>
       </main>
+      
     </div>
+    <footer className='paint__footer'>
+        <div>
+          <h5>SUPPORT</h5>
+          <span>F.I.F.A</span>
+          <span>PRIVACY POLICY</span>
+          <span>TERMS AND CONDITIONS</span>
+          <span>ACCESSIBILITY</span>
+        </div>
+        <div>
+          <h5>FOLLOW US</h5>
+          <span>INSTAGRAM</span>
+          <span>PINTEREST</span>
+          <span>FACEBOOK</span>
+          <span>NEWSLETTER</span>
+        </div>
+        <div>
+          <h5>PAINTBOX</h5>
+          <span>OUR STUDIO</span>
+          <span>CAREERS</span>
+          <span>PRESS</span>
+          <span>CONTACT US</span>
+        </div>
+      </footer>
+    </>
   )
 }
 
