ef34d8e7bba9763e5cc28fc5a990dd4d8463e2e8
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-04-06 19:04:09 +0900

    Add zoo page main

--- src/zoo/zoo.js
+++ src/zoo/zoo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss'
 function Zoo(){
+
   return(
     <div className='zoo__container'>
       <header>
@@ -15,6 +16,98 @@ function Zoo(){
           <li>tous</li>
         </ul>
     </header>
+    <main>
+      <article>
+        <img src="https://source.unsplash.com/random/300x200" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x201" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x202" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x203" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x204" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x205" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x206" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x207" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x208" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x109" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x100" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x101" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x202" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x203" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+      <article>
+        <img src="https://source.unsplash.com/random/300x204" />
+        <div>
+          <h3>Moco Montpellier Contemporain</h3>
+        </div>
+      </article>
+    </main>
     </div>
   )
 }
