9fe5ed7216ea0ec7190302e81b7497f83aca0bfe
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-04-07 21:20:49 +0900

    Add thumbnail in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './main.scss';
+import horroImg from './horroImg.png'
+import paintImg from './paintThumbnail.png';
+import zooImg from './zooThumbnail.png';
+import tennineImg from './tennineThumbnail.png';
+
 
 function MainPage(){
   return(
     <div className='mainpage'>
       <ul>
-        <li> <Link to="/horro"><button> Horro page </button> </Link> </li>
-        <li> <Link to="/paint"><button> Paint page </button> </Link> </li>
-        <li> <Link to="/zoo"><button> Z.O.O page </button> </Link> </li>
-        <li> <Link to='tennine'> <button> TenNine page </button> </Link> </li>
+        <li> 
+          <Link to="/horro" className='link'>
+            <img  src={horroImg} />
+            <button> Horro page </button>
+           </Link> 
+        </li>
+        <li> 
+          <Link to="/paint" className='link'>
+            <img  src={paintImg} />
+            <button>Paint page </button>
+           </Link> 
+        </li><li> 
+          <Link to="/zoo" className='link'>
+            <img  src={zooImg} />
+            <button>Z.O.O page </button>
+           </Link> 
+        </li><li> 
+          <Link to="/tennine" className='link'>
+            <img  src={tennineImg} />
+            <button>TenNine page </button>
+           </Link> 
+        </li>
       </ul>
     </div>
   )
